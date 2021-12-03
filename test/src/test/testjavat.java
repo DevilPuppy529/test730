8c978844a5b43afc1eca66235302dda07aeff5de
minji <minji@192.168.0.7>
2021-12-03 14:53:52 +0900

    메소드 추가함

--- test/src/test/testjavat.java
+++ test/src/test/testjavat.java
@@ -4,4 +4,9 @@ public class testjavat {
 	public static void main(String[] args) {
 		System.out.println("Hello Java World!!");
 	}
+	
+	@Override
+	public String toString() {
+		System.out.println("메소드 생성!");
+	}
 }
